bda92996a39ea12e1c14252fce76994d5e19ef72
juan-cantero <juan.cantero@outlook.com>
2020-02-10 11:12:18 -0300

    queue added

--- Data_Structure_Practice/Program.cs
+++ Data_Structure_Practice/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructure_Practice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -51,10 +52,49 @@ namespace Datastructure_Practice
             
         }
 
+        public static Queue<int> Reverse(Queue<int> queue)
+        {
+            var tempList = new Stack<int>();
+            for (int i = 0; i < queue.Count; i++)
+            {
+                var temp = queue.Dequeue();
+                tempList.Push(temp);
+            }
+            while (tempList.Count > 0)
+            {
+                queue.Enqueue(tempList.Pop());
+            }
+
+            return queue;
+        }
+
+
+
         static void Main(string[] args)
         {
-            bool isBalanced = IsBalancedExpression("[}]{[1]}");
-            Console.WriteLine(isBalanced);
+            try
+            {
+                var queue = new QueueArray<int>();
+                queue.Enqueue(4);
+                queue.Enqueue(5);
+                queue.Enqueue(6);
+                Console.WriteLine(queue.Front + " " + queue.Rear +" " + queue.Size);
+                Console.WriteLine(queue.IsFull());
+                
+                queue.Enqueue(33);
+                queue.Enqueue(33);
+                queue.Enqueue(33);
+                Console.WriteLine("----------------");
+                queue.PrintQueue();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+
+
 
         }
     }
---
+++ Data_Structure_Practice/Queue/QueueArray.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataStructure_Practice
+{
+    class QueueArray<T>
+    {
+        private T[] _list = new T[5];
+
+        public int Size
+        {
+            get { return _list.Length; }
+        }
+        public int Front { get; private set; }
+        public int Rear { get; private set; }
+
+        public QueueArray()
+        {
+            Front = 0;
+            Rear = 0;
+        }
+
+        public bool IsFull()
+        {
+            return (Front == 0 && Rear == Size) ||
+                GetNextRearPos() == Front;
+        }
+
+        private int GetNextRearPos() =>
+            (Rear + 1) % (Size );
+        private int GetNextFrontPos() =>
+            (Front + 1) % (Size );
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty");
+            Front = GetNextFrontPos();
+            return _list[Front - 1];
+        }
+
+        public void Enqueue(T element)
+        {
+            if (IsFull())
+                throw new InvalidOperationException("Queue is full");
+            
+            _list[Rear] = element;
+            Rear = GetNextRearPos();
+            
+
+        }
+
+
+        public bool IsEmpty()
+        {
+            return Front == Rear;
+        }
+
+        public void PrintQueue()
+        {
+            foreach (var element in _list)
+                Console.WriteLine(element);
+
+        }
+
+
+
+
+    }
+}
